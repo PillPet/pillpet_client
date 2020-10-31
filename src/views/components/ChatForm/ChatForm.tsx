d5b5fa6ad21ff422cfc02e9a478103ff266b1ae3
Pinot. Kim <pinot.kim@kakao.com>
2020-10-31 09:04:47 +0900

    :sparkles: 디버그용 기능들을 추가하라

--- src/views/components/ChatForm/ChatForm.tsx
+++ src/views/components/ChatForm/ChatForm.tsx
@@ -3,6 +3,8 @@ import React, {useCallback, useRef} from 'react';
 import css from './ChatForm.module.scss';
 import {useDispatch} from 'react-redux';
 import {setMessage, setChatLog} from '@store/slices/chatSlice';
+import classcat from 'classcat';
+import {author, contentType} from '@constants/types'
 
 interface ChatFormProps {}
 
@@ -11,46 +13,91 @@ const defaultMessage = "질문할 내용을 입력해주세요.";
 const useSubmit = (message : string) => {
     const dispatch = useDispatch();
     const textRef = useRef<HTMLInputElement>();
+
+    const userRef = useRef<HTMLSelectElement | author>();
+    const typeRef = useRef<HTMLSelectElement | contentType>();
+    const checkBoxRef = useRef<HTMLTextAreaElement | string>();
+
     const onSubmit = useCallback((evt) => {
         evt.preventDefault();
-        const text = textRef.current.value.trim();
+        const text = textRef.current?.value.trim();
         textRef.current.value = "";
+        console.log(text,
+                    author[userRef.current?.value],
+                    contentType[typeRef.current?.value],
+                    JSON.parse(checkBoxRef.current?.value));
+        if (!text) {
+            return;
+        }
         if (text === message) {
             return;
         }
         dispatch(setMessage(text));
-        dispatch(setChatLog({
-            author: "USER",
-            contentType: "TEXT",
-            text,
-            timestamp: +new Date(),
-            checkbox: []
-        }));
+        if (process.env.NODE_ENV === 'development') {
+            dispatch(setChatLog({
+                author: Number.parseInt(userRef.current?.value),
+                contentType: Number.parseInt(typeRef.current?.value),
+                text,
+                timestamp: +new Date(),
+                checkbox: JSON.parse(checkBoxRef.current?.value)
+            }))
+        } else {
+            dispatch(setChatLog({
+                author: author.USER,
+                contentType: contentType.TEXT,
+                text,
+                timestamp: +new Date(),
+                checkbox: []
+            }));
+        }
     }, [message]);
     return {
         textRef,
+        userRef,
+        typeRef,
+        checkBoxRef,
         onSubmit
     }
 };
 
 const ChatForm: React.FC<ChatFormProps> = () => {
     const {message} = useChatState();
-    const {textRef, onSubmit} = useSubmit(message);
-    console.log(message)
+    const {textRef, userRef, typeRef, checkBoxRef, onSubmit} = useSubmit(message);
+    const isDev = process.env.NODE_ENV === "development";
     return (
-        <form className={css.ChatForm} onSubmit={onSubmit}>
-            <input
-                ref={textRef}
-                className={css.textInput}
-                placeholder={defaultMessage}
-                type="text"
-            />
-            <div className={css.buttonWrap}>
-                <button type="submit">
-                    전송
-                </button>
+        <div className={css.form}>
+            <div className={classcat([isDev ? css.debug : css.none])}>
+                <div>
+                    <select ref={userRef}>
+                        <option value={author.USER}>USER</option>
+                        <option value={author.BOT}>BOT</option>
+                    </select>
+                </div>
+                <div>
+                    <select ref={typeRef}>
+                        <option value={contentType.TEXT}>TEXT</option>
+                        <option value={contentType.MIXIN}>MIXIN</option>
+                        <option value={contentType.LIST}>LIST</option>
+                    </select>
+                </div>
+                <div>
+                <textarea ref={checkBoxRef} defaultValue="[]"></textarea>
+                </div>
             </div>
-        </form>
+            <form className={css.ChatForm} onSubmit={onSubmit}>
+                <input
+                    ref={textRef}
+                    className={css.textInput}
+                    placeholder={defaultMessage}
+                    type="text"
+                />
+                <div className={css.buttonWrap}>
+                    <button type="submit">
+                        전송
+                    </button>
+                </div>
+            </form>
+        </div>
     )
 };
 
