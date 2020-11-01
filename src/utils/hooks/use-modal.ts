96d907156e164c333c338a1e262b848ff720cd03
Pinot. Kim <pinot.kim@kakao.com>
2020-11-01 23:23:22 +0900

    :sparkles: use-modal hooks를 추가하라

---
+++ src/utils/hooks/use-modal.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import * as bodyScrollLock from 'body-scroll-lock';
+
+export const useModal = (active: boolean, ref?) => {
+    const [, forceRender] = useState();
+    useEffect(() => {
+        if (active && ref?.current) {
+            bodyScrollLock.disableBodyScroll(ref.current);
+        } else if (!active && ref?.current) {
+            bodyScrollLock.enableBodyScroll(ref.current);
+        }
+    }, [active, ref?.current]);
+    useEffect(() => {
+        setTimeout(() => forceRender(null), 20);
+    }, []);
+}
