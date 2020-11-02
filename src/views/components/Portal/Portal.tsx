1e5f07523befbf4a2cb7d4d2d19c5d64f5b34236
Pinot. Kim <pinot.kim@kakao.com>
2020-11-02 10:11:24 +0900

    :sparkles: modal-container 베이스를 추가하라

---
+++ src/views/components/Portal/Portal.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { createPortal } from "react-dom";
+import css from './Portal.module.scss';
+import classcat from "classcat";
+
+interface PortalProps { }
+
+const useClientRendering = () => {
+    const [isClientRendering, setClientRendering] = useState(false);
+    useEffect(() => setClientRendering(true), []);
+    return isClientRendering;
+}
+
+const Portal: React.FC<PortalProps> = ({ children }) => {
+    const element = useMemo(() => (typeof document === 'object' ? document : undefined)?.getElementById('modal-root'), []);
+    if (useClientRendering()) {
+        return createPortal(children, element);
+    }
+    return null;
+}
+
+export const PortalWrap = ({ children, className, ...rest }) => {
+    return (
+        <div className={classcat([css.Portal, className || ''])} {...rest}>
+            {children}
+            <div className={css.dimmed} />
+        </div>
+    );
+};
+
+export default Portal;
